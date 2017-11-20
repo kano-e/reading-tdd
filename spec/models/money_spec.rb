18dd46e1ce525b4132b298832a9a78d41ea6cba8
Eri Takano <e-takano@feedforce.jp>
2017-11-20 20:20:03 +0900

    p26 Compare a result with a instance of Dollar
    `#amount` を使わずに Dollar のインスタンスを直接比較する

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Money Test', type: :model do
   it 'Test Multiplication' do
     five = Dollar.new(5)
     product = five.times(2)
-    expect(product.amount).to eq 10
+    expect(product).to eq Dollar.new(10)
     product = five.times(3)
     expect(product.amount).to eq 15
   end
