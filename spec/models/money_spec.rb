eb088ac88ebabb48d7c6bbe22c6f515ed624f4ca
Eri Takano <e-takano@feedforce.jp>
2017-12-14 21:28:43 +0900

    p51 Use Money.franc
    Money.dollar と同じように Money.franc も追加したい、という気持ちのテストを追加。

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe 'Money Test', type: :model do
   it 'Test Equality' do
     expect(Money.dollar(5)).to eq Money.dollar(5)
     expect(Money.dollar(5)).not_to eq Money.dollar(6)
-    expect(Franc.new(5)).to eq Franc.new(5)
-    expect(Franc.new(5)).not_to eq Franc.new(6)
-    expect(Franc.new(5)).not_to eq Money.dollar(5)
+    expect(Money.franc(5)).to eq Money.franc(5)
+    expect(Money.franc(5)).not_to eq Money.franc(6)
+    expect(Money.franc(5)).not_to eq Money.dollar(5)
   end
 
   it 'Test Franc Multiplication' do
-    five = Franc.new(5)
-    expect(five.times(2)).to eq Franc.new(10)
-    expect(five.times(3)).to eq Franc.new(15)
+    five = Money.franc(5)
+    expect(five.times(2)).to eq Money.franc(10)
+    expect(five.times(3)).to eq Money.franc(15)
   end
 end
