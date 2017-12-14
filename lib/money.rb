8d1aaf8132d4bce78b7c49a81213203dac7688a6
Eri Takano <e-takano@feedforce.jp>
2017-12-14 21:08:20 +0900

    p49 Add `.dollar` method for Money
    テストの通りに実装

--- lib/money.rb
+++ lib/money.rb
@@ -1,6 +1,10 @@
 class Money
   attr_accessor :amount
 
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
   def ==(money)
     amount == money.amount && self.instance_of?(money.class)
   end
