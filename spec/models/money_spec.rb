89e5e385449252e31351de5881bbe85bfe2d72d9
Eri Takano <e-takano@feedforce.jp>
2017-12-14 21:26:47 +0900

    p50 User Money.dollar
    Dollar を直接呼ぶ必要がなくなった

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -4,16 +4,16 @@ require 'franc'
 RSpec.describe 'Money Test', type: :model do
   it 'Test Multiplication' do
     five = Money.dollar(5)
-    expect(five.times(2)).to eq Dollar.new(10)
-    expect(five.times(3)).to eq Dollar.new(15)
+    expect(five.times(2)).to eq Money.dollar(10)
+    expect(five.times(3)).to eq Money.dollar(15)
   end
 
   it 'Test Equality' do
-    expect(Dollar.new(5)).to eq Dollar.new(5)
-    expect(Dollar.new(5)).not_to eq Dollar.new(6)
+    expect(Money.dollar(5)).to eq Money.dollar(5)
+    expect(Money.dollar(5)).not_to eq Money.dollar(6)
     expect(Franc.new(5)).to eq Franc.new(5)
     expect(Franc.new(5)).not_to eq Franc.new(6)
-    expect(Franc.new(5)).not_to eq Dollar.new(5)
+    expect(Franc.new(5)).not_to eq Money.dollar(5)
   end
 
   it 'Test Franc Multiplication' do
